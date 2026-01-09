0062e1a375b9966fb17c06e3406230f90b19849b
whiteduke <zef1rflis@yandex.ru>
2026-01-09 21:11:36 +0500

    fix: build

--- src/page/Dashboard/components/index.ts
+++ src/page/Dashboard/components/index.ts
@@ -2,4 +2,3 @@ export { CalendarSection } from "./CalendarSection";
 export { TeamWidget, type Team } from "./TeamWidget";
 export { ActivityWidget, type Activity } from "./ActivityWidget";
 export { NewCalendar } from "./NewCalendar";
-export { NotesList } from "./NotesList";
